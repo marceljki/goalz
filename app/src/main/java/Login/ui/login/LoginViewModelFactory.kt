506860edc2b764f2683968e87d15cd2278506c27
marceljki <marceljki@gmail.com>
2023-04-12 18:27:04 +0200

    Add login

---
+++ app/src/main/java/Login/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package Login.ui.login
+
+import Login.data.LoginDataSource
+import Login.data.LoginRepository
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
